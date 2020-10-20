9bf57128c85696b81bdd0775047597e2a0418c1b
wangbo_geetest <wangbo@geetest.com>
2020-10-20 10:43:28 +0800

    修改轮询时间间隔

--- geetest_config.js
+++ geetest_config.js
@@ -4,7 +4,7 @@ const GeetestConfig = Object.freeze({
     REDIS_HOST: "127.0.0.1",
     REDIS_PORT: "6379",
     BYPASS_URL: "http://bypass.geetest.com/v1/bypass_status.php",
-    CYCLE_TIME: 5,
+    CYCLE_TIME: 10,
     GEETEST_BYPASS_STATUS_KEY: "gt_server_bypass_status"
 })
 
